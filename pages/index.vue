67fb7461cffb8e3245d941afc575c1ecf9beb84f
Nuno Costa <nunocosta@Nunos-Air.netcabo.pt>
2023-09-28 21:56:02 +0100

    CHORE: Create modal and animation

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,21 @@
 <template>
-  <div class="app-overlord bg-slate-800 w-full flex justify-center">
+  <div
+    class="app-overlord relative bg-slate-800 w-full flex justify-center overflow-hidden"
+  >
     <div
-      class="bg-slate-700 border-2 rounded w-80 h-4/5 m-auto p-8 flex flex-col"
+      class="main-container relative bg-slate-700 border-2 rounded w-80 h-4/5 m-auto p-8 flex flex-col"
     >
+      <transition name="modal">
+        <template v-if="isModalOpen">
+          <div
+            id="create_modal"
+            class="todo-create-modal absolute bg-slate-300 left-full h-full w-full bg-slate-200"
+          >
+            <!-- ? Search how to left 100% when isModalOpen goes active. This can only happen after the component is loaded -->
+            <!-- ? Modal goes off to the right then on create click bring the modal inwards. -->
+          </div>
+        </template>
+      </transition>
       <h2
         class="w-full flex justify-center text-xl text-white font-mono grow-0"
       >
@@ -16,13 +29,10 @@
       <button
         class="button relative bg-slate-400 border-1 rounded h-8 w-full grow-0 text-slate-200"
         type="button"
+        @click="toggleModal"
       >
         Create
       </button>
-    </div>
-
-    <div id="create_modal" class="todo-create-modal">
-      <!-- ? Modal goes off to the right then on create click bring the modal inwards. -->
     </div>
   </div>
 </template>
@@ -30,11 +40,17 @@
 export default {
   data() {
     return {
+      isModalOpen: false,
       newTodo: "",
       todos: [],
     };
   },
   methods: {
+    //Toggle Modal
+    toggleModal() {
+      this.isModalOpen = !this.isModalOpen;
+      console.log(this.isModalOpen);
+    },
     //Create
     createTodo() {},
     //Delete
